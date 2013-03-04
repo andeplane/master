5e3cb4fc32a723538d62ee28495d2e06e97befc8
Anders Hafreager <anders@tapcat.is>
2013-03-04 11:49:08 +0100

    Added settings

---
+++ code/cpp/src/settings.cpp
@@ -0,0 +1,26 @@
+#include "settings.h"
+
+Settings::Settings(string filename) {
+    ini_file.load(filename);
+
+    load_previous_state = ini_file.getbool("load_previous_state");
+    create_movie = ini_file.getbool("create_movie");
+    statistics_interval = ini_file.getint("statistics_interval");
+    movie_every_n_frame = ini_file.getint("movie_every_n_frame");
+    number_of_particles = ini_file.getint("N");
+    timesteps = ini_file.getint("timesteps");
+    temperature = ini_file.getdouble("temperature");
+    wall_temperature = ini_file.getdouble("wall_temperature");
+    dt_factor = ini_file.getdouble("dt_factor");
+    acceleration = ini_file.getdouble("acceleration");
+    max_x_acceleration = ini_file.getdouble("max_x_acceleration");
+    density = ini_file.getdouble("density");
+    diam = ini_file.getdouble("diam");
+    Lx = ini_file.getdouble("Lx");
+    Ly = ini_file.getdouble("Ly");
+    Lz = ini_file.getdouble("Lz");
+    dt = ini_file.getdouble("dt");
+    cells_x = ini_file.getint("cells_x");
+    cells_y = ini_file.getint("cells_y");
+    cells_z = ini_file.getint("cells_z");
+}
