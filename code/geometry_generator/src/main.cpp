50d670be0e29b299681fd54d9a047c67c0c94c53
Anders Hafreager <anders@tapcat.is>
2013-10-14 14:13:53 +0200

    Creating normals and tangents, remember that

--- code/geometry_generator/src/main.cpp
+++ code/geometry_generator/src/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
     double threshold = ini.getdouble("threshold");
 
     ComplexGeometry cg;
-    cg.create_perlin_geometry(100, 100, 100, 4.13524,3.215,2.13531,3, threshold, true, 1);
+    cg.create_perlin_geometry(100, 100, 100, 4,3,2,3, threshold, true, 1);
     // cg.create_empty_space(100, 100, 100, true, 1.0);
     cg.save_to_file_2("./perlin4/",CVector(1,1,1));
     return 0;
